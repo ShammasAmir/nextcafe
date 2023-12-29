224fb4a4feeb4a70ab1284c92b584d7065eb10ba
ShammasAmir <amirshammas@gmail.com>
2023-12-29 11:45:49 +0330

    Create templates for index page.

---
+++ components/templates/index/Contact.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section className="book_section layout_padding">
+      <div className="container">
+        <div className="heading_container">
+          <h2>تماس با ما</h2>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="form_container">
+              <form action="">
+                <div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="نام و نام خانوادگی"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="ایمیل"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="موضوع پیام"
+                  />
+                </div>
+                <div>
+                  <textarea
+                    rows="10"
+                    style={{height: 100 + 'px'}}
+                    className="form-control"
+                    placeholder="متن پیام"
+                  ></textarea>
+                </div>
+                <div className="btn_box">
+                  <button>ارسال پیام</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="map_container ">
+              <div id="map" style={{height: 345 + 'px'}}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import About from "@/components/templates/index/About";
+import CardArea from "@/components/templates/index/CardArea";
+import Contact from "@/components/templates/index/Contact";
+import Food from "@/components/templates/index/Food";
+import Slider from "@/components/templates/index/Slider";
 import React from "react";
 
 function index() {
-  return <h1>INDEX-PAGE</h1>;
+  return (
+    <>
+      <Slider />
+      <CardArea />
+      <Food />
+      <About />
+      <Contact />
+    </>
+  );
 }
 
 export default index;
